40d49963c0bfbef256a4f32ddc62047dc91980c4
David Sanders <dsanderscanada@gmail.com>
2016-11-15 22:07:24 -0500

    Update php pages

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <!-- Initial PHP Script -->
         <?php 
 			error_reporting(E_ALL);
-			ini_set('display_errors', 'On');
+			ini_set('display_errors', 'Off');
 
             /*
              * Set the include path for all includes on this page.
